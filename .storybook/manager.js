53b546ed00a33ff4c2b2ac5f1f36901f55b354a5
yjb94 <jbyun94@naver.com>
2020-09-10 09:51:38 +0900

    chore: add storybook theme

---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+
+import { addons } from '@storybook/addons';
+import theme from '../src/theme';
+
+addons.setConfig({
+  theme: theme,
+});
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+
+  colorPrimary: '#3bb2b8',
+  colorSecondary: '#38cfac',
+
+  // Text colors
+  textColor: '#2d2c2c',
+
+  brandTitle: 'Lucky pear',
+  brandUrl: 'https://luckypear.io',
+  brandImage: 'https://lh6.googleusercontent.com/aoBDSVC95_MdAaxqPQTwst6PitXPHFq8FiFYuOD2AF86MV_yWnWr1DZT0JQYW-Q8DJ8TvhuSUeq5IcmNyfDI=w3360-h1796-rw',
+});
