faa3ca23fe70ad49f2c685355d165b511dd05bca
yjb94 <jbyun94@naver.com>
2020-09-10 10:36:07 +0900

    feat: add emotion theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GlobalStyle } from "../src/styles/globalStyle";
+import { ThemeProvider } from 'emotion-theming'
+import theme from '../src/styles/theme';
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Story />
-    </>
+    </ThemeProvider>
   ),
 ];
---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    primary: '#3bb2b8',
+    secondary: '#38CFAC',
+    
+    darkGrey: "#2d2c2c",
+    grey: "#C0C0C0",
+    lightGrey: "#fafafa",
+  
+    alert: '#e02020',
+    
+    white: '#ffffff',
+    transparent: 'transparent',
+  }
+}
+
+export type ThemeType = typeof theme;
+export default theme;
