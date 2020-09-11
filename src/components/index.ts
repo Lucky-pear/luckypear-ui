77f85522795af92760ca313d58d912d93687593a
yjb94 <jbyun94@naver.com>
2020-09-11 21:15:35 +0900

    fix: export button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import { withTheme } from '../hoc/withTheme'
 
 import { default as TextComponent } from './Text'
+import { default as ButtonComponent } from './Button'
 
 export const Text = withTheme(TextComponent);
+export const Button = withTheme(ButtonComponent);
